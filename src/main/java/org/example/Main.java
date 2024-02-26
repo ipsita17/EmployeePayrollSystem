05d23fadcd1fde92a80c75f0d6f21ccc8bd78de9
ipsita <ipsita.panda@accionlabs.com>
2024-02-26 11:14:05 +0530

    complete successfully

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PayrollSystem payrollSystem = new PayrollSystem();
+        FullTimeEmployee emp1 = new FullTimeEmployee("Jyoti" ,101, 40000.0);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Sipun", 102, 8, 300.0);
+
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+        System.out.println("Initial Employee Details: ");
+        payrollSystem.displayEmployee();
+        System.out.println("Removing Employees ");
+        payrollSystem.removeEmployee(102);
+        System.out.println("Remaining Employee Details: ");
+        payrollSystem.displayEmployee();
     }
 }
---
+++ src/main/java/org/example/PayrollSystem.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+
+
+public class PayrollSystem {
+    private ArrayList<Employee> employeeList;
+    public PayrollSystem() {
+        employeeList =new ArrayList<>();
+
+    }
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+    public void removeEmployee(int id) {
+        Employee employeeToRemove = null;
+        for(Employee employee :employeeList){
+            if(employee.getId()==id)
+            {
+                employeeToRemove = employee;
+                break;
+            }
+        }
+        if(employeeToRemove!=null){
+            employeeList.remove(employeeToRemove);
+        }
+    }
+
+    public void displayEmployee(){
+        for(Employee employee : employeeList){
+            System.out.println(employee);
+        }
+    }
+}
